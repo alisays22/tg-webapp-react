1e8d6ed627cfe1f0bd39531b405e0657200232aa
3sL5T3zXXxFm <hansuvarov@gmail.com>
2023-12-05 14:10:28 +0400

    add toggle

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './comonents/hooks/useTelegram';
 
-const tg = window.Telegram.WebApp;
+// const tg = window.Telegram.WebApp;
 
 
 
