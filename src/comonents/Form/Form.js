3fccb7c966223ec254601b9d20b2cc461fec2c39
3sL5T3zXXxFm <hansuvarov@gmail.com>
2023-12-08 16:45:25 +0400

    send data

--- src/comonents/Form/Form.js
+++ src/comonents/Form/Form.js
@@ -9,36 +9,37 @@ const [street, setStreet] = useState("");
 const [subject, setSubject] = useState("physical");
 const {tg} = useTelegram()
 
-const onSendData = useCallback(()=>{
-const data = {
-    country,
-    street,
-    subject,
-}
-tg.sendData(JSON.stringify(data));
-}, [])
-
-useEffect(() =>{
-    tg.WebApp.onEvent('mainButtonClicked', onSendData)
-    return () =>{
-        tg.WebApp.offEvent('mainButtonClicked', onSendData)
-    }
-        },[])
+const onSendData = useCallback(() => {
+    const data = {
+      country,
+      street,
+      subject,
+    };
+    tg.sendData(JSON.stringify(data));
+  }, [country, street, subject, tg]);
 
 
-    useEffect(() =>{
-tg.MainButton.setParams({
+useEffect(() => {
+  tg.WebApp.onEvent('mainButtonClicked', onSendData);
+  return () => {
+    tg.WebApp.offEvent('mainButtonClicked', onSendData);
+  };
+}, [onSendData, tg.WebApp]);
+
+
+  useEffect(() => {
+  tg.MainButton.setParams({
     text: "Отправить данные"
-}) 
-    },[tg.MainButton])
+  });
+}, [tg.MainButton]);
 
-    useEffect(() =>{
-       if(!street || !country ){
-        tg.MainButton.hide()
-       }else{
-        tg.MainButton.show()
-       }
-            },[country, street, tg.MainButton]) 
+useEffect(() => {
+  if (!street || !country) {
+    tg.MainButton.hide();
+  } else {
+    tg.MainButton.show();
+  }
+}, [country, street, tg.MainButton]);
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value);
